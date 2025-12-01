04e5527e58f555e5de65c907a205276a04917a38
Server Linux Bot by FedeFemia <server-bot@fedefemia.com>
2025-12-01 02:00:02 +0000

    Auto-Backup DB: 2025-12-01 02:00

--- backup_db.sql
+++ backup_db.sql
@@ -28,7 +28,7 @@ CREATE TABLE `visitatori` (
   `nome` varchar(50) NOT NULL,
   `data_visita` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=150 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -186,7 +186,8 @@ INSERT INTO `visitatori` VALUES
 (146,'Utente Web','2025-11-29 07:49:21'),
 (147,'Utente Web','2025-11-29 08:47:25'),
 (148,'Utente Web','2025-11-29 10:07:07'),
-(149,'Utente Web','2025-11-29 11:07:04');
+(149,'Utente Web','2025-11-29 11:07:04'),
+(150,'Utente Web','2025-11-30 04:50:50');
 /*!40000 ALTER TABLE `visitatori` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -199,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-30  2:00:02
+-- Dump completed on 2025-12-01  2:00:02
