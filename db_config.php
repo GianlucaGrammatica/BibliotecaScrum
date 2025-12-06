bc29105194cf5da635bef9d35f69c245f11a481d
Marco Colombara <colombaramarco21@gmail.com>
2025-12-06 11:43:40 +0100

    Sistemo il db config

--- db_config.php
+++ db_config.php
@@ -9,35 +9,9 @@ $db   = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASSWORD'];
 
-class Database
-{
-
-    private static $instance = null;
-    private $pdo;
-
-
-    private function __construct()
-    {
-        try {
-            $this->pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die("Errore connessione DB: " . $e->getMessage());
-        }
-    }
-
-    public static function getInstance()
-    {
-        if (self::$instance === null) {
-            self::$instance = new Database();
-        }
-        return self::$instance;
-    }
-
-    public function getConnection()
-    {
-        return $this->pdo;
-    }
-
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Errore connessione DB: " . $e->getMessage());
 }
-
