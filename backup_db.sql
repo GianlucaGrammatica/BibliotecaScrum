81222b9dae00f5dcc9d67bac6a6c8b975cd78887
Server Linux Bot by FedeFemia <server-bot@fedefemia.com>
2026-01-27 02:00:03 +0000

    Auto-Backup DB: 2026-01-27 02:00

--- backup_db.sql
+++ backup_db.sql
@@ -291,7 +291,7 @@ CREATE TABLE `notifiche` (
   KEY `codice_alfanumerico` (`codice_alfanumerico`),
   KEY `idx_dataora_scadenza` (`dataora_scadenza`),
   CONSTRAINT `notifiche_ibfk_1` FOREIGN KEY (`codice_alfanumerico`) REFERENCES `utenti` (`codice_alfanumerico`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -668,4 +668,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-26  2:00:03
+-- Dump completed on 2026-01-27  2:00:03
