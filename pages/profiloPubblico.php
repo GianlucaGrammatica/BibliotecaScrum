4d6ebbd302c3b8d61daca47edffb297e8f515c72
Federico <federico.femia121007@gmail.com>
2026-01-31 09:20:55 +0100

    profilo pubblico fixes

--- pages/profiloPubblico.php
+++ pages/profiloPubblico.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 ini_set('error_log', '/var/www/html/php_errors.log');
 
 require_once __DIR__ . '/../vendor/autoload.php';
